a4efa242ad2b402b5fd5a6e1abb9b9a40d076cb1
Shayne Shaw <shayne.shaw@gmail.com>
2020-04-07 20:50:06 +0100

    Missing scripts

---
+++ mazegame/Assets/Scripts/Maze/LevelCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelCounter : MonoBehaviour
+{
+    int levelCounter = 0;
+    public Text levelText;
+
+    public void OnNextLevel()
+    {
+        levelCounter++;
+        levelText.text = levelCounter.ToString();
+    }
+
+    public void ResetLevel()
+    {
+        levelCounter = 0;
+        levelText.text = levelCounter.ToString();
+    }
+
+    public void Start()
+    {
+        OnNextLevel();
+    }
+
+}
